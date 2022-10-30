73cfb079c5871b9ce5e738c7bb0afe7fd966f90b
GABRIEL EUGÊNIO EUGÊNIO <>
2022-10-30 19:47:32 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Media-e-soma-de-idade-e-salario/function.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void limpeza(){
+  printf("************************************\n");
+}
+
---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include "function.h"
+/*Faça um programa que leia n pessoas solicitando dados como:idade,salário
+armazenando em vetores e que mostre a media de idade e salario*/
+
+int main(void) {
+  int escolha,quantidade;
+  printf("Quantidade máxima: 100\n");
+  printf("Quantidade de idades e salário que deseja inserir: ");
+  scanf("%d", &quantidade);
+  calcular(quantidade);  
+  menu(escolha);
+  scanf("%d", &escolha);
+  return 0;
+}
